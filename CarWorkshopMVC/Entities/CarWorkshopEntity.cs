f945999d3b391334d5efd782341bcc19a02886bd
Marcin-gif <63023436+Marcin-gif@users.noreply.github.com>
2024-08-10 19:23:21 +0200

    Add project files.

---
+++ CarWorkshopMVC/Entities/CarWorkshopEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CarWorkshopMVC.Entities
+{
+    public class CarWorkshopEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default;
+        public string? Description { get; set; }
+        public string Address { get; set; } = default;
+        public string? PhoneNumber { get; set; }
+    }
+}
